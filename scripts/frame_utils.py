19ca08c93a1eb66ed9f9d2bc4e9218fdbe0c8a3f
Rafael <138421856+JavaRaf@users.noreply.github.com>
2025-02-15 01:41:04 -0300

    refactor: otimizar o workflow do GitHub:
    Troca image magick pelo pillow

--- scripts/frame_utils.py
+++ scripts/frame_utils.py
@@ -1,10 +1,6 @@
-import os
-import random
-import subprocess
-from pathlib import Path
-
 from PIL import Image
-
+import random
+from pathlib import Path
 from scripts.load_configs import load_configs, load_frame_counter
 from scripts.logger import get_logger
 from scripts.paths import episodes_dir, frames_dir
@@ -46,54 +42,39 @@ def build_frame_file_path(frame_number: int) -> tuple[Path, int, int]:
 
 def random_crop_generator(frame_path: str, frame_number: int) -> tuple[str, str]:
     """
-
     Gera um recorte aleatório de um frame.
-
 
     Args:
         frame_path: Caminho do arquivo do frame
         frame_number: Número do frame
-        config: Dicionário de configuração
 
     Returns:
         tuple[str, str]: (caminho do arquivo gerado, mensagem descritiva)
     """
-
+    
     min_x = load_configs().get("posting")["random_crop"].get("min_x")
     min_y = load_configs().get("posting")["random_crop"].get("min_y")
 
     crop_width = crop_height = random.randint(min_x, min_y)  # min_x = 200, min_y = 600
 
     with Image.open(frame_path) as img:
-        image_width = img.width
-        image_height = img.height
+        image_width, image_height = img.size
 
-    crop_x = random.randint(0, 65535) % (image_width - crop_width)
-    crop_y = random.randint(0, 65535) % (image_height - crop_height)
+        # Calculando o recorte aleatório
+        crop_x = random.randint(0, image_width - crop_width)
+        crop_y = random.randint(0, image_height - crop_height)
 
-    output_crop_path = episodes_dir / "temp_crop.jpg"
+        # Realizando o recorte
+        cropped_img = img.crop((crop_x, crop_y, crop_x + crop_width, crop_y + crop_height))
 
-    command = [
-        "magick" if os.name == "nt" else "convert",
-        frame_path,
-        "-crop",
-        f"{crop_width}x{crop_height}+{crop_x}+{crop_y}",
-        output_crop_path,
-    ]
+        # Caminho para salvar a imagem recortada
+        output_crop_path = Path(episodes_dir) / "temp_crop.jpg"
 
-    try:
-        subprocess.run(command, check=True, capture_output=True, text=True)
+        # Salvando a imagem recortada
+        cropped_img.save(output_crop_path)
 
-        message = (
-            f"Random Crop. [{crop_width}x{crop_height} ~ X: {crop_x}, Y: {crop_y}]"
-        )
-        return output_crop_path, message
-    except subprocess.CalledProcessError as e:
-        logger.error(f"Erro ao executar ImageMagick: {e.stderr}", exc_info=True)
-        return None
-    except Exception as e:
-        logger.error(f"Erro ao executar ImageMagick: {e}", exc_info=True)
-        return None
+        message = f"Random Crop. [{crop_width}x{crop_height} ~ X: {crop_x}, Y: {crop_y}]"
+        return str(output_crop_path), message
 
 
 def get_total_episode_frames(episode_number: int) -> int:
